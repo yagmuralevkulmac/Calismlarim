fed7bc15025f93110143985ac07752c1226d8255
yagmuralevkulmac <yaqmurklmc@gmail.com>
2024-07-02 09:38:23 +0300

    Çalışmalarım
    Calismalarım

---
+++ JS/js.js
@@ -0,0 +1,20 @@
+function kare(x){
+    return x*x;
+}
+
+function kup(y){
+    return y*y*y;
+}
+
+function topla(a,b){
+    return a+b;
+}
+
+function carp(a,b){
+    return a*b;
+}
+
+  //fonksiyon tanımla
+  function yaz(deger){
+    document.write(deger+"<br>");
+}
